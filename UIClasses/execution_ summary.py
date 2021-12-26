b5168ac0db826d9a2a3d6fba5be936b3229d4d4a
Anto <52914169+AntoData@users.noreply.github.com>
2021-12-26 21:22:56 +0100

    Updated execution summary

--- UIClasses/execution_ summary.py
+++ UIClasses/execution_ summary.py
@@ -4,7 +4,6 @@ sys.path.append("../Config")
 import LoggerMeta
 from window_design import SimpleWindow
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 import os
 from question_windows_handler import QuestionnaireWindowHandler
@@ -19,17 +18,24 @@ class ExecutionSummary(SimpleWindow):
         total_questions: int = len(CurrentSession.current_session)
         correct_questions = [x for x in CurrentSession.current_session.keys() if CurrentSession.current_session[x]]
         n_correct_questions: int = len(correct_questions)
-        qualification: int = round((n_correct_questions/total_questions) * 100,1)
+        qualification: float = round((n_correct_questions/total_questions) * 100,1)
+        self.colour_font: str = ""
+        if 70.0 > qualification >= 50.0:
+            self.colour_font = "#FF5733"
+        elif qualification >= 70.0:
+            self.colour_font = "#128301"
+        else:
+            self.colour_font = "#BF1602"
         self.qualification_font = ("TkDefaultFont", 30)
-        self.label_qualification: tk.Label = tk.Label(self.window,text="You got {0}%".format(qualification),
-                                                      font=self.qualification_font)
+        self.label_qualification: tk.Label = tk.Label(self.window, text="You got {0}%".format(qualification),
+                                                      font=self.qualification_font, foreground=self.colour_font)
         self.label_qualification.pack(anchor=tk.CENTER)
         tk.Frame(self.window, height=20).pack()
         print(qualification)
-        scrollbar = Scrollbar(self.window)
-        scrollbar.pack(side=RIGHT, fill=Y)
+        scrollbar = tk.Scrollbar(self.window)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.item_font = ("TkDefaultFont", 10)
-        mylist = Listbox(self.window, yscrollcommand=scrollbar.set, width=400, font=self.item_font)
+        mylist = tk.Listbox(self.window, yscrollcommand=scrollbar.set, width=400, font=self.item_font)
         i: int = 0
         for vkey in CurrentSession.current_session.keys():
             result: str = ""
@@ -37,16 +43,15 @@ class ExecutionSummary(SimpleWindow):
                 result = "Correct"
             else:
                 result = "Failed"
-            mylist.insert(END, "{0} − {1} − {2}".format(i, vkey, result))
+            mylist.insert(tk.END, "{0} − {1} − {2}".format(i, vkey, result))
             i += 1
 
-        mylist.pack(side=LEFT, fill=BOTH, anchor=CENTER)
+        mylist.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.CENTER)
         scrollbar.config(command=mylist.yview)
 
-        mainloop()
 
 if __name__ == "__main__":
-    CurrentSession.current_session["What do you say to a person in the morning?"] = True
+    CurrentSession.current_session["What do you say to a person in the morning?"] = False
     CurrentSession.current_session["What of the following ones are Python basic types?"] = False
     CurrentSession.current_session["Which of the following countries belong to the European Union"] = True
     w = ExecutionSummary()
