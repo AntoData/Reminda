aad52f75e876fa0cd0dadff3f4f4ed106dad8b57
Anto <52914169+AntoData@users.noreply.github.com>
2021-12-28 23:13:00 +0100

    Finished question creation

--- UIClasses/question_creation.py
+++ UIClasses/question_creation.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+import sys
+sys.path.append("../BE-Logic")
+sys.path.append("../Config")
+from Questionnaire import QuestionnaireClass
+from Current_session import CurrentSession
+from QuestionLogic import QuestionClass
 from window_design import SimpleWindow
 import tkinter as tk
 
@@ -17,7 +25,7 @@ class QuestionCreation(SimpleWindow):
     def check_correct_number_checked(self):
         if self.intVars is not None:
             r = len([x.get() for x in self.intVars if x.get() != 0])
-            return r == 2
+            return r == 2 or r == 1
         else:
             return True
 
@@ -34,7 +42,7 @@ class QuestionCreation(SimpleWindow):
         self.but.grid(column=5, row=0)
         self.frame2 = tk.Frame(self.window)
         self.frame2.pack()
-        self.button_save = tk.Button(self.frame2, text="Save", state=tk.DISABLED)
+        self.button_save = tk.Button(self.frame2, text="Save", state=tk.DISABLED, command=self.button_save_handler)
         self.button_exit = tk.Button(self.frame2, text="Exit")
         self.button_save.grid(row=0, column=0)
         self.button_exit.grid(row=0, column=1)
@@ -84,22 +92,37 @@ class QuestionCreation(SimpleWindow):
             self.check_boxes.append(checkbox)
             self.intVars.append(check_box_var_i)
         self.button_less = tk.Button(self.frame2, text="Show -", command=self.show_less_handler)
-        self.button_less.grid(row=0,column=3)
+        self.button_less.grid(row=0, column=3)
 
     def show_less_handler(self):
         self.frame.destroy()
         self.frame2.destroy()
         self.single_answer_display()
 
-    def check_button_save_question(self, event):
-        print("NEW ONE")
-        print(self.question_filled())
-        print(self.check_correct_number_checked())
-        print(self.all_answers_filled_in())
+    def check_button_save_question(self):
         if self.question_filled() and self.check_correct_number_checked() and self.all_answers_filled_in():
             self.button_save["state"] = tk.NORMAL
         else:
             self.button_save["state"] = tk.DISABLED
+        self.question_text.after(1, self.check_button_save_question)
+
+    def gather_answers(self):
+        answers_str: (str | [(str, bool)]) = []
+        if self.check_boxes is not None:
+            i: int = 0
+            for stringVarAnswer in self.answers_stringVars:
+                answers_str.append((stringVarAnswer.get(), self.intVars[i].get() == 1))
+                i += 1
+        else:
+            answers_str = self.answer_stringVar1.get()
+        return answers_str
+
+    def button_save_handler(self):
+        answers_str: str | [(str, bool)] = self.gather_answers()
+        question_str: str = self.question_text.get("1.0", "end-1c").strip()
+        self.question_result = QuestionClass(question_str, answers_str)
+        print(self.question_result)
+        self.window.destroy()
 
     def __init__(self, title):
         SimpleWindow.__init__(self, 600, 500, title)
@@ -108,7 +131,7 @@ class QuestionCreation(SimpleWindow):
         self.question_label: tk.Label = tk.Label(self.window, text="Question", font=self.font)
         self.question_label.pack()
         self.question_text: tk.Text = tk.Text(self.window, width=57, height=7)
-        self.question_text.bind("<Key>", self.check_button_save_question)
+        self.question_text.after(1, self.check_button_save_question)
         self.question_text.pack()
         tk.Frame(self.window, height=20).pack()
         self.answer_stringVar = tk.StringVar("")
@@ -129,6 +152,7 @@ class QuestionCreation(SimpleWindow):
         self.check_box_1 = None
         self.answers = None
         self.intVars = None
+        self.question_result: QuestionClass = None
         self.single_answer_display()
 
 
