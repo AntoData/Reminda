e59b849c72912fe6e083e72ac867a0ab42f017e4
Anto <52914169+AntoData@users.noreply.github.com>
2022-01-02 21:52:27 +0100

    Documentation done for Current_session.py

--- BE-Logic/Current_session.py
+++ BE-Logic/Current_session.py
@@ -3,6 +3,22 @@ import LoggerMeta
 
 @LoggerMeta.class_decorator_logger("INFO")
 class CurrentSession(metaclass=LoggerMeta.MetaLogger):
+    """
+    This class is used to represent a session when we are answering the questions of a questionnaire
+
+    ...
+
+    Attributes
+    ----------
+    current_session : {str: bool}
+        Class attribute: A dictionary it has a string key which is the question and a boolean which is True if we
+        answered the question correctly
+
+    Methods
+    -------
+    delete_session(cls)
+        Class method used to restart a session (cleaning the dictionary)
+    """
     current_session: {str: bool} = {}
 
     @classmethod
